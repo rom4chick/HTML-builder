b1ebfc8a517cf727d21a2ff1c217c1cf72e49935
rom4chick <obereg2005.2002@gmail.com>
2022-05-23 18:35:36 +0300

    feat: add file write

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
+
+let data = '';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -9,13 +13,29 @@ console.log('Input something:');
 
 rl.on('SIGINT', () => {
   console.log('You\'ve ended input');
+  fs.writeFile(path.resolve(__dirname, 'text.txt'), data, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
   rl.close();
 });
 
 rl.on('line', (input) => {
   if (input === 'exit') {
     console.log('You\'ve ended input');
+    fs.writeFile(path.resolve(__dirname, 'text.txt'), data, (err) => {
+      if (err) {
+        console.log(err);
+      }
+    });
     rl.close();
+  } else if (data === ''){
+    data += input;
+  } else {
+    data += `\n${input}`;
   }
 });
+
+
 
